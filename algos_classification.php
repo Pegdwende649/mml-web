177d3ff2445b682fa8d58c592b75ebf3eb124a6e
Carloss1998-lab <calebcarloss@gmail.com>
2021-03-15 23:15:39 +0100

    position

--- algos_classification.php
+++ algos_classification.php
@@ -6,46 +6,17 @@
 
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="icon" type="image/png" href="images/deep1.png" />
-      <link href="http://netdna.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-      <meta charset="utf-8">
-      <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-      <script src="http://netdna.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-      <link href="https://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet">
-
 
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title> Deep </title>
-      <link rel="stylesheet" href="style_pg.css">
-
+      
 
       <style> 
-body{margin-top:20px;}
-header {
-  background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(https://www.getsmarter.com/disk/public/sBBGDTboLgihDAZHFNC7aFHj/mit_sloan_csail_machine_learning_course_page_large_header_banner.jpg);
-  height: 10vh;
-  background-size: 30%;
-  background-position: center center;
-  position: relative;
-}
-
-.content {
-      width: 94%;
-      margin: 4em auto;
-      font-size: 20px;
-      line-height: 30px;
-      text-align: justify;
-}
-          .carousel-caption {
-              position: relative;
-              left: 0;
-              top: 15%;}
-
           .carousel-item {
           height: 100vh;
           min-height: 350px;
-          background: no-repeat center center scroll;
           -webkit-background-size: cover;
           -moz-background-size: cover;
           -o-background-size: cover;
@@ -53,10 +24,9 @@ header {
 
 
           .select-algo-class {
-            position: r;
-            width: 600px;
-            height: 100px;
-            margin: 20% 30%;
+ 
+ 
+            margin: 12% 15%;
             text-align: center;
           }
           .select-algo-class h1 {
